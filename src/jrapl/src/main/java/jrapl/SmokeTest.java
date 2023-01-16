19b48a227abfd6743eab332a6247b197fd851835
atpoverload <atpoverload@gmail.com>
2023-01-16 15:54:19 -0700

    another fix

--- src/jrapl/src/main/java/jrapl/SmokeTest.java
+++ src/jrapl/src/main/java/jrapl/SmokeTest.java
@@ -196,7 +196,7 @@ final class SmokeTest {
                     - Durations.toMicros(
                         Timestamps.between(powercap.getStart(), powercap.getEnd())))
                     / 1000000,
-                raplReadings.getReadingList()
+                raplReadings.values()
                     .stream()
                     .map(
                         r -> String.format(
